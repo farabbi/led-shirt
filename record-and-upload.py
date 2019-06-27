cdd5df61f93ba78067df90f69826027c9397bb8c
jiuxin <farabbi@163.com>
2019-06-27 14:54:22 -0400

    commit test

--- record-and-upload.py
+++ record-and-upload.py
@@ -11,8 +11,3 @@ while(1):
     sleep(60)
     camera.stop_recording()
     call(["bash","upload.sh"])
-    
-#timestr = strftime("%Y%m%d-%H%M%S")
-#camera.start_recording('/home/pi/Desktop/videos/%s.h264' % timestr)
-#sleep(30)
-#camera.stop_recording()
