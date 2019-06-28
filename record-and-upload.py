2f79402e7ee937cffe3a21c4d52981e7e1a844d1
jiuxin <farabbi@163.com>
2019-06-27 22:20:23 -0400

    A-2019-06-27-222023

--- record-and-upload.py
+++ record-and-upload.py
@@ -7,7 +7,7 @@ camera.resolution = (128,128)
 
 while(1):
     #timestr = strftime("%Y%m%d-%H%M%S")
-    camera.start_recording('videos/A.h264')
+    camera.start_recording('/home/pi/A.h264')
     sleep(60)
     camera.stop_recording()
     call(["bash","upload.sh"])
--- upload.sh
+++ upload.sh
@@ -1,3 +1,4 @@
+cp ~/A.h264 videos/
 git add *
 git commit -m "A-$(date +"%F-%H%M%S")"
 git pull --rebase
