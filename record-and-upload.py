9a73d206c853ca351c5f4bedbbdddc6708357da8
RPi 1 <farabbi@163.com>
2019-07-02 13:44:48 -0400

    A-2019-07-02-134448

--- record-and-upload.py
+++ record-and-upload.py
@@ -1,9 +1,13 @@
+# python3
+# Each video last for 1 minutes
+# The code is cumtomized for RPi 1 (A)
+
 from picamera import PiCamera
 from time import sleep, strftime
 from subprocess import call
 
 camera = PiCamera()
-camera.resolution = (128,128)
+camera.resolution = (128,64)
 
 while(1):
     #timestr = strftime("%Y%m%d-%H%M%S")
--- upload.sh
+++ upload.sh
@@ -1,5 +1,9 @@
+# bash
+# Sync videos folder with remote
+# The code is customized for RPi 1 (A)
+
 git add *
 git commit -m "A-$(date +"%F-%H%M%S")"
 git pull --rebase
 git push -u origin master
-cp -r videos/ ~/Desktop/
+cp -r videos ~/Desktop
