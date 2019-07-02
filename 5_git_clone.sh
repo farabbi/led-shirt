153cf9a71abfd22a8931f5cd5826508149f5d6ac
RPi 1 <farabbi@163.com>
2019-07-02 14:36:34 -0400

    add git clone bash shell

---
+++ 5_git_clone.sh
@@ -0,0 +1,7 @@
+# bash
+# Download 3 repos from GitHub: led-shirt, led-shirt-videos and rpi-rgb-led-matrix
+
+git clone git@github.com:farabbi/led-shirt.git
+git clone git@github.com:farabbi/led-shirt-videos.git
+git clone https://github.com/hzeller/rpi-rgb-led-matrix.git
+
